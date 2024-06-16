9724bddac34ac6bea174508b1eb4ff825b57a710
Achmad96 <totsukamail3278@gmail.com>
2024-06-16 22:27:20 +0700

    modified file

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -9,17 +9,16 @@ export default async function Page() {
     <Container className={"flex items-center justify-center max-sm:mb-10"}>
       <div className="flex h-full w-[90%] items-center justify-around max-md:flex-col max-sm:gap-3">
         <div className="-z-50 w-[50%] max-lg:w-[75%] max-md:w-[90%] max-sm:w-full">
-          {/* <div className="mask-profile -z-50"> */}
           <div className="relative -left-8 h-[30rem] w-full max-md:left-0 max-sm:h-[17rem]">
             <Image
               src={`${fileUrl}&id=${profileId}`}
               alt="profile-remas"
               className="drop-shadow-[0_35px_35px_rgba(0,0,0,0.7)]"
               fill={true}
+              priority={true}
               sizes="(max-width: 1280px) 100vw"
             />
           </div>
-          {/* </div> */}
         </div>
         <ProfileHeroContent />
       </div>
