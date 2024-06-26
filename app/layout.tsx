d54ceb6b1375080cee74dfdbca20a6acc668922e
Achmad96 <totsukamail3278@gmail.com>
2024-06-26 10:22:03 +0700

    modified files

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "@/components/ui/Navbar";
 import NextProgressBar from "@/components/ui/NextProgressBar";
 import Footer from "@/components/ui/Footer";
 
