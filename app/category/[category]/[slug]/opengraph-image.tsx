9b8c163a88caeaec5c5f64ca55c8acb11df949de
Achmad96 <totsukamail3278@gmail.com>
2024-06-26 10:40:53 +0700

    modified files

--- app/category/[category]/[slug]/opengraph-image.tsx
+++ app/category/[category]/[slug]/opengraph-image.tsx
@@ -1,10 +1,12 @@
+import { ArticlePageType } from "@/app/types";
+import { getSingleArticlePage } from "@/utils/notion-service";
 import { ImageResponse } from "next/og";
 
 // Route segment config
 export const runtime = "edge";
 
 // Image metadata
-export const alt = "Remaja Masjid";
+export const alt = "Image Cover";
 export const size = {
   width: 1200,
   height: 630,
@@ -13,42 +15,24 @@ export const size = {
 export const contentType = "image/png";
 
 // Image generation
-export default async function Image() {
-  // Font
-  const interSemiBold = fetch(
-    new URL("./Inter-SemiBold.ttf", import.meta.url),
-  ).then((res) => res.arrayBuffer());
-
+export default async function Image({ params }: any) {
+  const { article } = (await getSingleArticlePage(
+    params.slug,
+  )) as ArticlePageType;
   return new ImageResponse(
     (
       // ImageResponse JSX element
       <div
         style={{
           fontSize: 128,
-          background: "white",
+          backgroundImage: `url('${article.cover}')`,
           width: "100%",
           height: "100%",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
         }}
-      >
-        Remaja Masjid
-      </div>
+      />
     ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-      fonts: [
-        {
-          name: "Inter",
-          data: await interSemiBold,
-          style: "normal",
-          weight: 400,
-        },
-      ],
-    },
   );
 }
